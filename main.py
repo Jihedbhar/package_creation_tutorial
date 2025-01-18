9249d4d76282be2e7c56afa459d2a2e4e52e6ca5
Jihed Bhar <bharjihed@gmail.com>
2025-01-18 16:40:42 +0100

    add failing test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from package_creation_tutorial.string_ops import (reverse_string, 
                                                   count_vowels, capitalize_words)
 
+
+
+
+
 def main() -> None:
     """
     Main function to demonstrate the string operations.
@@ -13,5 +17,5 @@ def main() -> None:
     print(f"Capitalized: {capitalize_words(test_string)}")
 
 if __name__ == "__main__":
-    print("Hi! PARIS")
+    
     main()
